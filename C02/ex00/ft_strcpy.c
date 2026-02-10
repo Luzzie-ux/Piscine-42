ed7911659d7b5c4d0ffc64d812ba7e36ea957f0f
Rodrigo Pereira <rodrpere@c1r13s9.42porto.com>
2026-02-10 15:55:35 +0000

    C02

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/09 17:51:22 by rodrpere          #+#    #+#             */
+/*   Updated: 2026/02/09 19:30:17 by rodrpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	char	*start;
+
+	start = dest;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/09 18:47:32 by rodrpere          #+#    #+#             */
+/*   Updated: 2026/02/09 19:24:53 by rodrpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	size_t	size;
+
+	size = 0;
+	while (size < n && src[size] != '\0')
+	{
+		dest[size] = src[size];
+		size++;
+	}
+	while (size < n)
+	{
+		dest[size] = '\0';
+		size++;
+	}
+	return (dest);
+}
