6c44620b0837c25a2d84fd00b3092d7a8133a92e
Rodrigo Pereira <rodrpere@c1r6s9.42porto.com>
2026-02-12 16:42:21 +0000

    more stuff

--- examples/05/iterative.c
+++ examples/05/iterative.c
@@ -3,20 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   iterative.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rodrpere <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 14:19:58 by rodrpere          #+#    #+#             */
-/*   Updated: 2026/02/12 14:32:18 by rodrpere         ###   ########.fr       */
+/*   Updated: 2026/02/12 16:07:50 by rodrpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int	main(void)
 {
 	int n = 0;
 	int fact = 1;
-	printf("Enter a Number:");
+	printf("Enter a Number: ");
 	scanf("%d\n", &n);
 
 	while (n >= 1)
@@ -25,7 +26,7 @@ int	main(void)
 		n --;
 	}
 
-	printf("factorial: %d\n", fact)
+	printf("factorial: %d\n", fact);
 
 	return 0;
 }
