59c2b66edcce198538f011c9d86df2a987bc5714
Rodrigo Pereira <rodrpere@c1r6s9.42porto.com>
2026-02-12 16:41:00 +0000

    boa sorte

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/12 14:45:33 by rodrpere          #+#    #+#             */
+/*   Updated: 2026/02/12 16:40:03 by rodrpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	fact;
+
+	fact = 1;
+	if (nb <= -1)
+	{
+		return (0);
+	}
+	while (nb >= 1)
+	{
+		fact = (nb * fact);
+		nb--;
+	}
+	return (fact);
+}
+
+/*int	main(void)
+{
+	printf("%d\n",ft_iterative_factorial(5));
+	printf("%d\n",ft_iterative_factorial(0));
+	printf("%d\n",ft_iterative_factorial(-48494));
+	printf("%d\n",ft_iterative_factorial(2));
+}*/
---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/12 16:28:52 by rodrpere          #+#    #+#             */
+/*   Updated: 2026/02/12 16:39:43 by rodrpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb <= -1)
+		return (0);
+	if (nb == 1 || nb == 0)
+		return 1;
+	return nb * ft_recursive_factorial(nb - 1);
+}
+
+/*int	main(void)
+{
+	int n = 0;
+	int fact = 1;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	
+	fact = ft_recursive_factorial(n);
+
+	printf("factorial:%d\n", fact);
+
+	return (0);
+}*/
