14aa9fee829530dc85acebcd45ecb7b0b739006e
Luzzie-ux <estelarodrigoluz@gmail.com>
2026-02-14 15:35:07 +0000

    prime

---
+++ C05/ex06/ft_is_prime.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_prime.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 15:01:01 by rodrpere          #+#    #+#             */
+/*   Updated: 2026/02/14 15:32:17 by rodrpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_is_prime(int nb)
+{
+	int div;
+
+	div = 2;
+	if (nb <= 1)
+		return (0);
+	while (div < nb)
+	{
+		if (nb % div == 0)
+			return (0);
+		div++;
+	}
+	return (1);
+}
+
+int main (void)
+{
+	int n;
+	printf("Choose a number: ");
+	scanf("%d", &n);
+	int result = ft_is_prime(n);
+	
+	printf("%d is prime: %d", n, result);
+}
