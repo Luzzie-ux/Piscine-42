536783fdb05bf589e6232cc9e74a263317354a39
Rodrigo Pereira <rodrpere@c1r15s7.42porto.com>
2026-02-16 11:10:57 +0000

    C03

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,80 +6,46 @@
 /*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 12:48:58 by rodrpere          #+#    #+#             */
-/*   Updated: 2026/02/11 18:08:13 by rodrpere         ###   ########.fr       */
+/*   Updated: 2026/02/16 10:55:47 by rodrpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-/*char	*ft_strstr(char *str, char *to_find)
-{
-	int	i;	int	j;
-	
-	i = 0;	j = 0;
-	if(!str || !to_find) 
-		return (0);
-	if(to_find[0] == '\0')
-		return (str);
-	while (str[i] != '\0')
-	{
-		while (to_find[j] != '\0')
-		{
-			if(str[i + j] == '\0' || str[i + j] != to_find[j])
-			{
-				break;
-			}
-			++j;
-		}
-		++i;
-		if(to_find[j] == '\0') 
-			return (&str[i - 1]);
-	}
-	return (0);
-}*/
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
+#include <stdio.h>
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	size_t	i;
-	size_t	j;
-	size_t	len_a;
-	size_t	len_b;
+	int	i;
+	int	j;
 
 	i = 0;
-	j = 0;
-	len_a = ft_strlen(str);
-	len_b = ft_strlen(to_find);
-	if (len_a < len_b)
-		return (0);
-	if (to_find[j] == '\0')
+	if (to_find[0] == '\0')
 		return (str);
 	while (str[i] != '\0')
 	{
-		while (to_find[j] == str[i + j])
+		j = 0;
+		while (str[i + j] != '\0' && to_find[j] != '\0'
+			&& str[i + j] == to_find[j])
 		{
-			if (to_find[j] == str[i])
-				return (&str[i]);
 			j++;
 		}
+		if (to_find[j] == '\0')
+			return (&str[i]);
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
-/*#include <stdio.h>
-int	main(void)
+
+/*int	main(void)
 {
 	char str[] = "Testando testando 123";
-	char to_find[] = "tando";
+	char to_find[] = "";
+	char str1 [] = "Estudando muito";
+	char to_find1[] = "xyz";
+	char str2[] = "A bee shouldnt be able to fly";
+	char to_find2[] = "able";
 
 	printf("%s\n", ft_strstr(str, to_find));
+	printf("%s\n", ft_strstr(str2, to_find2));
+	printf("%s\n", ft_strstr(str1, to_find1));
 }*/
