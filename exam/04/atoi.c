ab87ca0d74dc6b633772aed1629b00fb5f438ecf
Luzzie-ux <estelarodrigoluz@gmail.com>
2026-02-19 21:22:05 +0000

    atoi

--- exam/04/atoi.c
+++ exam/04/atoi.c
@@ -6,23 +6,50 @@
 /*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 16:02:14 by rodrpere          #+#    #+#             */
-/*   Updated: 2026/02/19 16:29:50 by rodrpere         ###   ########.fr       */
+/*   Updated: 2026/02/19 21:20:34 by rodrpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_atoi(const char *str);
+int	ft_atoi(char *str)
 {
-	int	i;
+	int		num;
+	int		sign;
+	char	*ptr;
 
-	i = 0;
-	while (str[i] != '\0')
+	num = 0;
+	sign = 1;
+	ptr = str;
+	while ((*ptr >= 9 && *ptr <= 13) || *ptr == 32)
+		ptr++;
+	if (*ptr == '-' && *ptr == '+')
 	{
-		if ((str[i] <= 9 && str[i] >= 13) || str[i] != 32)
-			i++;
-		else if (str[i] '+' && str[i] == '-')
-			i++;
-		else if (str[i] <= '0' && str[i])	
+		if (*ptr == '-')
+		{
+			sign = -1;
+		}
+		ptr++;
 	}
+	while (*ptr >= '0' && *ptr <= '9')
+	{
+        num = num * 10 + (*ptr - '0');
+        ptr++;
+    } 
+    return (num * sign);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+int main (void)
+{
+    char    str[] = "12345";
+    char    str1[] = "1 2 3 4 5 6 7 8 9 0 a b c";
+    char    str2[] = "+-123";
+
+    printf("Atoi is: %d", ft_atoi(str));
+    printf("\nAtoi is: %d", ft_atoi(str1));
+    printf("\nAtoi is: %d", ft_atoi(str2));
+	
+    return (0);
 }
